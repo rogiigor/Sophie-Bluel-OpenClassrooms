91451af26a100d756992a049c6339c5671fca045
Igor Vish <igorv0@yahoo.com>
2026-03-28 02:47:14 -0500

    edit button fix. Login/Logout button logic using javascript. Opening nad closing modal window, for now without gallery

--- FrontEnd/scripts/editing.js
+++ FrontEnd/scripts/editing.js
@@ -1,3 +1,5 @@
+import { renderDefaultAllWorks } from "./works.js";
+
 function setEditingMode() {
     const headerElement = document.querySelector("header");
     const projectsTitleElement = document.querySelector("#portfolio h2");
@@ -11,12 +13,15 @@ function setEditingMode() {
     if (authToken === null) {
         divHeader.classList.add("hidden");
         spanEditButton.classList.add("hidden");
+        handleLoginLink();
     } else {
         divHeader.classList.remove("hidden");
         spanEditButton.classList.remove("hidden");
         filters.classList.add("hidden");
         headerElement.before(divHeader);
         projectsTitleElement.append(spanEditButton);
+        handleLogoutLink();
+        handleEditButton();
     }
 }
 
@@ -34,19 +39,77 @@ function createEditHeader() {
 }
 
 function createEditButton() {
-    const spanElement = document.createElement("span");
-    spanElement.classList.add("edit-span");
+    const divElement = document.createElement("div");
+    divElement.classList.add("edit-div");
     const iconElement = document.createElement("i");
     iconElement.classList.add("far", "fa-edit", "icon-edit");
-    const button = document.createElement("button");
+    const button = document.createElement("btn-edit");
     button.classList.add("btn-edit");
     button.innerText = "Edit";
     button.classList.add("edit-button");
 
-    spanElement.appendChild(iconElement);
-    spanElement.appendChild(button);
+    divElement.appendChild(iconElement);
+    divElement.appendChild(button);
 
-    return spanElement;
+    return divElement;
+}
+
+function handleLoginLink() {
+    const headerList = document.querySelector(".header-list");
+    headerList.addEventListener("click", (event) => {
+        if (event.target.textContent == "login") {
+            window.location = "login.html";
+        }  
+    });
+}
+
+function handleLogoutLink() {
+    const headerList = document.querySelector(".header-list");
+    const logoutElement = document.createElement("li");
+    logoutElement.innerText = "logout";
+    logoutElement.classList.add("logout");
+    for (let i = 0; i < headerList.children.length; i++) {
+        let listElement = headerList.children[i];
+        if (listElement.innerText === "login") {
+            listElement.replaceWith(logoutElement);
+        }
+    }
+    logoutElement.addEventListener("click", (event) => {
+        const authToken = window.localStorage.getItem("token");
+        if (authToken !== null) {
+            window.localStorage.removeItem("token");
+            window.location = "index.html";
+        }
+    })
+}
+
+function handleEditButton() {
+    const editButton = document.querySelector("btn-edit");
+    editButton.addEventListener("click", (event) => {
+        console.log("Edit button was clicked");
+
+        openModalGallery(event);
+        addCloseModalGallery();
+    })
+}
+
+function openModalGallery(event) {
+    const modalGalleryDialog = document.querySelector(".modal-gallery");
+    modalGalleryDialog.showModal();
+    event.stopPropagation();
+}
+
+function addCloseModalGallery() {
+    const modalGalleryDialog = document.querySelector(".modal-gallery");
+    const closeModalButton = document.querySelector(".btn-close");
+    closeModalButton.addEventListener("click", () => {
+        modalGalleryDialog.close();
+    });
+    modalGalleryDialog.addEventListener("click", (event) => {
+        if (event.target === modalGalleryDialog) {
+            modalGalleryDialog.close();
+        }
+    })
 }
 
 export { setEditingMode };
